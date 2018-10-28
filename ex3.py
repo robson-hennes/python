62333447be728a612a85023365c9df56039007b6
Robson Hennes <robson.hennes@gmail.com>
2018-10-27 21:49:53 -0300

    Arquivos de exercícios Python
    Arquivos para estudos

---
+++ ex3.py
@@ -0,0 +1,8 @@
+def main():
+    num1 = input('Insira o primeiro número da soma')
+    num2 = input('Insira o segundo número da soma')
+    num1 = int(num1)
+    num2 = int(num2)
+    resultado = (num1 + num2)
+    print('O resultado é = ',resultado)
+main()
